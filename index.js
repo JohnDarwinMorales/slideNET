a925ebe1d24fdd6d62c9eb93eba4a6379d2a01b2
johndarwinmorales <jdmg44@hotmail.com>
2015-04-05 06:23:43 +0000

    commit preventivo

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var http = require("http");
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandler");
 
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo");
+var handle = {}
+handle["/"] = requestHandlers.iniciar;
+handle["/iniciar"] = requestHandlers.iniciar;
+handle["/subir"] = requestHandlers.subir;
 
-  response.end();
-}
-
-http.createServer(onRequest).listen(5555);
-console.log("server is rurning in port: 5555");
+server.iniciar(router.route, handle);
