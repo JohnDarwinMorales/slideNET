cf7379887fbbab03b2044d3c62a4ee2aa32ecf2c
jdmg_utp <jdmorales.utp@gmail.com>
2015-04-05 02:05:27 -0500

    agregando server.listen

--- config/server.js
+++ config/server.js
@@ -2,18 +2,25 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 var path = require("path");
+var io = require('socket.io');
 
-    function iniciar(route,handle) {
+function iniciar(route,handle) {
+    process.env.PORT=process.argv[2] || 5555;
+    process.env.IP= 'localhost';
 
-        function onRequest(request, response) {
-            var pathname = url.parse(request.url).pathname;
-            console.log("-GET [" + pathname + "]");
-            route(handle,pathname,response);
-        }
-
-        http.createServer(onRequest).listen(5555,function(){
-            console.log("server is rurning in port: 5555");
-        });
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("-GET [" + pathname + "]");
+        route(handle,pathname,response);
     }
+
+    var server=http.createServer(onRequest);
+
+    server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0",function(){
+        var addr= server.address();
+        console.log("Server listening at", addr.address +":"+ addr.port);
+    });
+
+}
 
 exports.iniciar = iniciar;
--- index.js
+++ index.js
@@ -9,17 +9,3 @@ handle["/subir"] = requestHandlers.subir;
 handle["error404"] = requestHandlers.error404;
 
 server.iniciar(router.route, handle);
-
-
-
-
-
-
-
-/*
- server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
- var addr= server.address();
- console.log("server is running at"+ addr.address+ ":" + addr.port);
- });
- */
-//console.log("server is rurning in port: 5555");
