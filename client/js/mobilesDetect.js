ecee0369b73a46923f9b1124863a4bd66285da63
johndarwinmorales <jdmg44@hotmail.com>
2015-04-06 06:02:31 +0000

    preventivo

--- client/js/mobilesDetect.js
+++ client/js/mobilesDetect.js
@@ -1,15 +1,26 @@
-var md = new MobileDetect(
-    'Mozilla/5.0 (Linux; U; Android 4.0.3; en-in; SonyEricssonMT11i' +
-    ' Build/4.1.A.0.562) AppleWebKit/534.30 (KHTML, like Gecko)' +
-    ' Version/4.0 Mobile Safari/534.30');
+var md = new MobileDetect(window.navigator.userAgent);
 
-console.log( md.mobile() );          // 'Sony'
-console.log( md.phone() );           // 'Sony'
-console.log( md.tablet() );          // null
-console.log( md.userAgent() );       // 'Safari'
-console.log( md.os() );              // 'AndroidOS'
-console.log( md.is('iPhone') );      // false
-console.log( md.is('bot') );         // false
-console.log( md.version('Webkit') );         // 534.3
-console.log( md.versionStr('Build') );       // '4.1.A.0.562'
-console.log( md.match('playstation|xbox') ); // false
+console.log( md.mobile() );
+console.log( md.phone() );
+console.log( md.tablet() );
+
+function load(){
+    var idMob= document.getElementById('mob');
+    idMob.innerText="phone:"+md.phone()+"tablet:"+md.tablet();
+   // alert("sadadasdas");
+}
+
+//idMobile.innerText= "phone:"+md.phone()+"tablet:"+md.tablet();
+
+/*
+ console.log( md.mobile() );          // 'Sony'
+ console.log( md.phone() );           // 'Sony'
+ console.log( md.tablet() );          // null
+ console.log( md.userAgent() );       // 'Safari'
+ console.log( md.os() );              // 'AndroidOS'
+ console.log( md.is('iPhone') );      // false
+ console.log( md.is('bot') );         // false
+ console.log( md.version('Webkit') );         // 534.3
+ console.log( md.versionStr('Build') );       // '4.1.A.0.562'
+ console.log( md.match('playstation|xbox') ); // false
+ */
--- config/eventHandlers_client.js
+++ config/eventHandlers_client.js
@@ -6,10 +6,16 @@ var connect_client=function(data){
 
 };
 
+var connect_client=function(data){
+
+};
+
 
 function onSocketConnection(socket){
     socket.on('connect',connect_client);
     socket.on('disconnect',disconnect_client);
+
+    socket.emit('date',{'date': new Date()});
 }
 
 
